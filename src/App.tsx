c7451c19cd33e9ebdc3bbc878b888129ddffe24c
Leandroyyy <leandro.cavallari123@gmail.com>
2022-07-05 00:54:31 -0300

    teste

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/pre-register' element={<RegisterProspect/>}/>
+        <Route path='pre-register' element={<RegisterProspect/>}/>
       </Routes>
     </BrowserRouter>
   )
